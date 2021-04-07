fd1acd38b0448e3f18ab3147ed3dea361f218dbb
tomo <tomohiroyoshida0511@gmail.com>
2021-04-07 22:26:12 +0900

    Update: use guard

--- homework/work_1.hs
+++ homework/work_1.hs
@@ -1,23 +1,23 @@
 -- 問題1
 myGcd :: Integer -> Integer -> Integer
 myGcd x 0 = x
-myGcd x y =
-  if x < y then myGcd y x
-  else myGcd y (x `mod` y)
+myGcd x y
+  | x < y = myGcd y x
+  | otherwise = myGcd y (x `mod` y)
 
 -- 問題2
 myRange :: Integer -> Integer -> [Integer]
-myRange x y =
-  if x > y then []
-  else if x == y then [x]
-  else x:myRange (x+1) y
+myRange x y
+  | x > y = []
+  | x == y = [x]
+  | otherwise = x:myRange (x+1) y
 
 -- 問題3
 myInsert :: Integer -> [Integer] -> [Integer]
 myInsert x [] = [x]
-myInsert x (y:ys) =
-  if x <= y then x:y:ys
-  else  y:myInsert x ys
+myInsert x (y:ys)
+  | x <= y = x:y:ys
+  | otherwise = y:myInsert x ys
 
 -- 問題4
 myIsort :: [Integer] -> [Integer]
