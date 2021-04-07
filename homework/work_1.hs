689fff4e4152c3c7a8355e671f255a4d34036e17
tomo <tomohiroyoshida0511@gmail.com>
2021-04-07 23:38:47 +0900

    Update: Integer => Int

--- homework/work_1.hs
+++ homework/work_1.hs
@@ -1,25 +1,25 @@
 -- 問題1
-myGcd :: Integer -> Integer -> Integer
+myGcd :: Int -> Int -> Int
 myGcd x 0 = x
 myGcd x y
   | x < y = myGcd y x
   | otherwise = myGcd y (x `mod` y)
 
 -- 問題2
-myRange :: Integer -> Integer -> [Integer]
+myRange :: Int -> Int -> [Int]
 myRange x y
   | x > y = []
   | x == y = [x]
   | otherwise = x:myRange (x+1) y
 
 -- 問題3
-myInsert :: Integer -> [Integer] -> [Integer]
+myInsert :: Int -> [Int] -> [Int]
 myInsert x [] = [x]
 myInsert x (y:ys)
   | x <= y = x:y:ys
   | otherwise = y:myInsert x ys
 
 -- 問題4
-myIsort :: [Integer] -> [Integer]
+myIsort :: [Int] -> [Int]
 myIsort [] = []
 myIsort (x:xs) = myInsert x (myIsort xs)
