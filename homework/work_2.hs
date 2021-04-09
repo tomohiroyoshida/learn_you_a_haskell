0057dadb14136e972e082b235374bdae34aae010
tomo <tomohiroyoshida0511@gmail.com>
2021-04-09 12:28:24 +0900

    Chore: change function name

--- homework/work_2.hs
+++ homework/work_2.hs
@@ -4,20 +4,10 @@ sumList [] = 0
 sumList xs = foldr (+) 0 xs
 
 -- 問題6
-myMap :: (a -> b) -> [a] -> [b]
-myMap _ [] = []
-myMap f (x:xs) = f x:myMap f xs
-
-myFilter :: (a -> Bool) -> [a] -> [a]
-myFilter _ [] = []
-myFilter p (x:xs)
-  | p x == False = myFilter p xs
-  | otherwise = x:myFilter p xs
-
 oddPlus1 :: [Int] -> [Int]
 oddPlus1 [] = []
 oddPlus1 xs =
-  myMap (+ 1) (myFilter (\x -> mod x 2 == 1) xs)
+  map (+1) [x | x <- xs, x `mod` 2 == 1]
 
 -- 問題7
 merge :: [Int] -> [Int] -> [Int]
@@ -29,10 +19,10 @@ merge (x:xs) (y:ys)
 
 -- 問題8
 split :: [Int] -> ([Int], [Int])
-split = snd . foldr go (False, ([], []))
+split = snd . foldr halve (False, ([], []))
   where
-    go e (False, (x, y)) = (True, (e:x, y))
-    go e (True, (x, y)) = (False, (x, e:y))
+    halve e (False, (x, y)) = (True, (e:x, y))
+    halve e (True, (x, y)) = (False, (x, e:y))
 
 -- 問題9
 msort :: [Int] -> [Int]
