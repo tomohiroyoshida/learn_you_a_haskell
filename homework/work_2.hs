d744393d93497e2fe4c0c9610ffa74973e1ca072
tomo <tomohiroyoshida0511@gmail.com>
2021-04-09 12:42:08 +0900

    Chore: change problem numbers

--- homework/work_2.hs
+++ homework/work_2.hs
@@ -1,15 +1,15 @@
--- 問題5
+-- 問題1
 sumList :: [Int] -> Int
 sumList [] = 0
 sumList xs = foldr (+) 0 xs
 
--- 問題6
+-- 問題2
 oddPlus1 :: [Int] -> [Int]
 oddPlus1 [] = []
 oddPlus1 xs =
   map (+1) [x | x <- xs, x `mod` 2 == 1]
 
--- 問題7
+-- 問題3
 merge :: [Int] -> [Int] -> [Int]
 merge xs [] = xs
 merge [] ys = ys
@@ -17,14 +17,14 @@ merge (x:xs) (y:ys)
   | x < y = x:merge xs (y:ys)
   | otherwise = y:merge (x:xs) ys
 
--- 問題8
+-- 問題4
 split :: [Int] -> ([Int], [Int])
 split = snd . foldr halve (False, ([], []))
   where
     halve e (False, (x, y)) = (True, (e:x, y))
     halve e (True, (x, y)) = (False, (x, e:y))
 
--- 問題9
+-- 問題5
 msort :: [Int] -> [Int]
 msort [] = []
 msort [x] = [x]
