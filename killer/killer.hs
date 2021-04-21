fe9cc3a991634d6b1662eb32d425b7957aad5c94
tomo <tomohiroyoshida0511@gmail.com>
2021-04-22 03:45:47 +0900

    Add cageFormula

--- killer/killer.hs
+++ killer/killer.hs
@@ -1,16 +1,4 @@
--- サンプル
-data Exp1 = Val1 Int | Plus1 Exp1 Exp1 | Times Exp1 Exp1
-
-instance Show Exp1 where
-    show (Val1 n)       = show n
-    show (Plus1 e1 e2)  = "(+ " ++ show e1 ++ " " ++ show e2 ++ ")"
-    show (Times e1 e2) = "(* " ++ show e1 ++ " " ++ show e2 ++ ")"
-
--- test1 = Plus1 (Val1 1) (Times (Val1 2) (Val1 3))
--- test2 = show test1
--- test3 = putStrLn test2
--- test4 = print test1
-
+import Data.List
 
 -- 論理式の実装
 data Exp = Var Int Int | Val Int | Plus [Exp]
@@ -21,31 +9,53 @@ data Formula = And [Formula]
 　　　　　　　　| Geq Exp Exp
 　　　　　　　　| Eq Exp Exp
 
+instance Show Exp where
+  show (Val n) = show n
+  show (Var i j) = "x" ++ show i ++ show j
+  show (Plus []) = []
+  show (Plus (e : es)) = "(+ " ++ show e ++ " " ++ showPls es ++ ")"
+
+instance Show Formula where
+  show (And []) = []
+  show (And (f : fs)) = "(and " ++ show f ++ " " ++ showFs fs ++ ")"
+  show (Or []) = []
+  show (Or (f : fs)) = "(or " ++ show f ++ " " ++ showFs fs ++ ")"
+  show (Distinct []) = []
+  show (Distinct (e : es)) = "(distinct " ++ show e ++ " " ++ showEs es ++ ")"
+  show (Geq e1 e2) = "(>= " ++ show e1 ++ " " ++ show e2 ++ ")"
+  show (Eq e1 e2) = "(= " ++ show e1 ++ " " ++ show e2 ++ ")"
+
 showEs :: [Exp] -> String
-showEs [] = ""
+showEs [] = []
 showEs [x] = show x
 showEs (x : xs) = show x ++ " " ++ showEs xs
 
 showFs :: [Formula] -> String
-showFs [] = ""
+showFs [] = []
 showFs [f] = show f
 showFs (f : fs) = show f ++ " " ++ showFs fs
 
-instance Show Exp where
-  show (Val n) = show n
-  show (Var i j) = "x" ++ show i ++ show j
-  show (Plus []) = ""
-  show (Plus (e : es)) = "(+" ++ " " ++ show e ++ " " ++ showEs es ++ ")"
+showPls :: [Exp] -> String
+showPls xs = intercalate " " [showEs xs]
 
-instance Show Formula where
-  show (And []) = ""
-  show (And (f : fs)) = "(and " ++ show f ++ " " ++ showFs fs ++ ")"
-  show (Or []) = ""
-  show (Or (f : fs)) = "(or " ++ show f ++ " " ++ showFs fs ++ ")"
-  show (Distinct []) = ""
-  show (Distinct (e : es)) = "(distinct " ++ show e ++ " " ++ showEs es ++ ")"
-  show (Geq e1 e2) = "(>= " ++ show e1 ++ " " ++ show e2 ++ ")"
-  show (Eq e1 e2) = "(= " ++ show e1 ++ " " ++ show e2 ++ ")"
+
+-- 数独ソルバー
+cageFormula :: [[Int]] -> String
+cageFormula ((xs)) = "(and " ++ showCFs xs ++ ")"
+
+showCFs :: [[Int]] -> String
+showCFs [] = []
+showCFs ([] : _) = []
+showCFs ((x : xs) : []) = show (Eq (Val x) (Plus (vars xs)))
+showCFs ((x : xs) : xss) =
+  show (Eq (Val x) (Plus (vars xs))) ++ " " ++ showCFs xss
+
+vars :: [Int] -> [Exp]
+vars [] = []
+vars (x : xs) = Var (x `div` 10) (x `mod` 10) : vars xs
+
+-- 各行に
+-- unique ::
 
 -- tests
 test1 = Var 1 2
@@ -59,3 +69,7 @@ test11 = Or [Geq (Val 1) (Val 2), Eq (Val 3) (Val 4)]
 test12 = Geq (Var 1 2) (Var 3 4)
 test13 = Eq (Val 1) (Var 2 3)
 test14 = Distinct [Val 1,Val 2,Val 3,Val 4,Val 5]
+test15 = And []
+test16 = Or []
+
+testa = cageFormula [[3, 11, 12], [15, 13, 14, 15]]
