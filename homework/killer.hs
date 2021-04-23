73dc722546058ffcd787f8be996da39b1f2cd491
tomo <tomohiroyoshida0511@gmail.com>
2021-04-23 20:09:48 +0900

    Add cageaFormula, uniqueCol, uniqueRow

--- homework/killer.hs
+++ homework/killer.hs
@@ -32,3 +32,45 @@ instance Show Formula where
   show (Distinct (e : es)) = "(distinct " ++ show e ++ " " ++ showEs es ++ ")"
   show (Geq e1 e2) = "(>= " ++ show e1 ++ " " ++ show e2 ++ ")"
   show (Eq e1 e2) = "(= " ++ show e1 ++ " " ++ show e2 ++ ")"
+
+-- 数独ソルバー
+cageFormula :: [[Int]] -> Formula
+cageFormula xs = And (showCFs xs)
+
+showCFs :: [[Int]] -> [Formula]
+showCFs [] = []
+showCFs ([] : _) = []
+showCFs ((x : xs) : []) = [Eq (Val x) (Plus (vars xs))]
+showCFs ((x : xs) : xss) = [Eq (Val x) (Plus (vars xs))]  ++ showCFs xss
+
+vars :: [Int] -> [Exp]
+vars [] = []
+vars (x : xs) = Var (x `div` 10) (x `mod` 10) : vars xs
+
+-- 行に被りなし
+rowFormula :: Formula
+rowFormula = And (uniqueRow 1)
+
+uniqueRow :: Int -> [Formula]
+uniqueRow n
+  | n == 9 = [Distinct (row 9 1)]
+  | otherwise = [Distinct (row n 1)] ++ uniqueRow (n+1)
+
+row :: Int -> Int -> [Exp]
+row i j
+  | j == 9 = [Var i 9]
+  | otherwise = Var i j : row i (j+1)
+
+
+-- 各列に被りがない
+colFormula :: Formula
+colFormula = And (uniqueCol 1)
+uniqueCol :: Int -> [Formula]
+uniqueCol n
+  | n == 9 = [Distinct (col 9 1)]
+  | otherwise = [Distinct (col 1 n)] ++ uniqueCol (n+1)
+
+col :: Int -> Int ->  [Exp]
+col i j
+  | i == 9 = [Var 9 j]
+  | otherwise =  Var i j : col (i+1) j
