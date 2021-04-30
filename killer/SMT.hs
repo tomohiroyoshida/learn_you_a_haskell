d65a2ab9e05c2c90ba6778a91bcf895a9463ed9e
tomo <tomohiroyoshida0511@gmail.com>
2021-04-30 15:45:42 +0900

    Impl solve func

--- killer/SMT.hs
+++ killer/SMT.hs
@@ -1,6 +1,34 @@
 import Text.ParserCombinators.Parsec
 import System.Process
 
+-- 論理式の実装
+data Exp = Var Int Int | Val Int | Plus [Exp]
+
+data Formula = And [Formula]
+              | Or [Formula]
+              | Distinct [Exp]
+              | Geq Exp Exp
+              | Eq Exp Exp
+
+instance Show Exp where
+  show (Val n) = show n
+  show (Var i j) = "x" ++ show i ++ show j
+  show (Plus []) = []
+  show (Plus (e : es)) = "(+ " ++ show e ++ " " ++ showEFs es ++ ")"
+
+instance Show Formula where
+  show (And []) = "true"
+  show (And (f : fs)) = "(and " ++ show f ++ " " ++ showEFs fs ++ ")"
+  show (Or []) = "false"
+  show (Or (f : fs)) = "(or " ++ show f ++ " " ++ showEFs fs ++ ")"
+  show (Distinct []) = ""
+  show (Distinct (e : es)) = "(distinct " ++ show e ++ " " ++ showEFs es ++ ")"
+  show (Geq e1 e2) = "(>= " ++ show e1 ++ " " ++ show e2 ++ ")"
+  show (Eq e1 e2) = "(= " ++ show e1 ++ " " ++ show e2 ++ ")"
+
+showEFs :: Show a => [a] -> String
+showEFs es = unwords [ show e | e <- es]
+
 -- scanners
 keyword :: String -> Parser ()
 keyword s = do
